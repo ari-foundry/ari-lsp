976f0da64eb8b43ea06cc31828c3b6a5d5dd4e7b
smc0478 <smc0478@naver.com>
2026-07-06 08:13:03 +0900

    Start smoke method value scan

--- scripts/check.sh
+++ scripts/check.sh
@@ -100,6 +100,8 @@ require_grep "SmokeMethodValueWaitFallback" src/json_rpc.ari
 require_grep "fallback_smoke_method_value_wait" src/json_rpc.ari
 require_grep "SmokeMethodValueScanFallback" src/json_rpc.ari
 require_grep "fallback_smoke_method_value_scan" src/json_rpc.ari
+require_grep "SmokeMethodValueScanStart" src/json_rpc.ari
+require_grep "start_smoke_method_value_scan" src/json_rpc.ari
 require_grep "SmokeRequestIdValueScanReset" src/json_rpc.ari
 require_grep "reset_smoke_request_id_value_scan" src/json_rpc.ari
 require_grep "SmokeRequestIdValueScanStart" src/json_rpc.ari
@@ -151,6 +153,7 @@ require_grep "waiting_method_colon = method_colon_wait_start.waiting_method_colo
 require_grep "waiting_method_value = method_value_wait_start.waiting_method_value" src/json_rpc.ari
 require_grep "method_key_match = method_value_wait_fallback.method_key_match" src/json_rpc.ari
 require_grep "method_key_match = method_value_scan_fallback.method_key_match" src/json_rpc.ari
+require_grep "reading_method_value = method_value_scan_start.reading_method_value" src/json_rpc.ari
 require_grep "reading_id_value = request_id_value_scan_reset.reading_id_value" src/json_rpc.ari
 require_grep "reading_id_value = request_id_value_scan_start.reading_id_value" src/json_rpc.ari
 require_grep "id_key_match = request_id_value_scan_fallback.id_key_match" src/json_rpc.ari
